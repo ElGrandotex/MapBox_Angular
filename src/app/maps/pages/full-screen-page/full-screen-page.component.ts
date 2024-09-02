4032706c63f04a15404a3465a6c6438d94304122
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-09-02 12:42:03 -0500

    Full screen page

---
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -0,0 +1,26 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
+
+@Component({
+  selector: 'app-full-screen-page',
+  templateUrl: './full-screen-page.component.html',
+  styleUrl: './full-screen-page.component.css'
+})
+export class FullScreenPageComponent implements AfterViewInit{
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if( !this.divMap) throw 'Elemento no encontrado'
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-78.5, -0.3], // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+  }
+
+}
